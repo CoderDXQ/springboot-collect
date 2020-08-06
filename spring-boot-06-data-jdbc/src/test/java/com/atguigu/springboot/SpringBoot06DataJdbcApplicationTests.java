9e821192b610820b74abd876b97360fb2737a448
cococolabear <794055465@qq.com>
2020-08-06 23:05:05 +0800

    JDBC自动配置原理

--- spring-boot-06-data-jdbc/src/test/java/com/atguigu/springboot/SpringBoot06DataJdbcApplicationTests.java
+++ spring-boot-06-data-jdbc/src/test/java/com/atguigu/springboot/SpringBoot06DataJdbcApplicationTests.java
@@ -1,13 +1,29 @@
 package com.atguigu.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBoot06DataJdbcApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    public void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 
 }
