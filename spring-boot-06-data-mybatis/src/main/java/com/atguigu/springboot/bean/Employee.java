9435dd228452922a1a960d29c377654b5bf0c829
cococolabear <794055465@qq.com>
2020-08-07 03:43:13 +0800

    整合Mybatis

---
+++ spring-boot-06-data-mybatis/src/main/java/com/atguigu/springboot/bean/Employee.java
@@ -0,0 +1,29 @@
+package com.atguigu.springboot.bean;
+
+public class Employee {
+    private Integer id;
+    private String lastName;
+    private Integer gender;
+    private String email;
+    private Integer dId;
+
+    public Integer getdId() {
+        return dId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
