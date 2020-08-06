18eb5d7a36b4970a87e13ff889b1c483b6b49323
cococolabear <794055465@qq.com>
2020-08-06 23:51:00 +0800

    JDBC自动配置原理

---
+++ spring-boot-06-data-jdbc/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    //数据库模板
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @GetMapping("/query")
+    public Map<String,Object> map(){
+        List<Map<String,Object>> list = jdbcTemplate.queryForList("SELECT * FROM department");
+        return list.get(0);
+    }
+    //如果Run之后查看网页localhost:8080/query返回500，那就是department表里数据被初始化了，在application.yml配置了initialization-mode: always的原因
+    //在navicat刷新或者重新启动一下数据库连接，在department表里重新添加数据然后查看网页即可
+}
