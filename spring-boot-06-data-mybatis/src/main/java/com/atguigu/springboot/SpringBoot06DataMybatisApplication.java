b6646f5a1ccdd4bf2088f639249005e64d827744
cococolabear <794055465@qq.com>
2020-08-07 23:25:24 +0800

    整合Mybatis，使用MapperScan批量扫描某个路径下的mapper接口

--- spring-boot-06-data-mybatis/src/main/java/com/atguigu/springboot/SpringBoot06DataMybatisApplication.java
+++ spring-boot-06-data-mybatis/src/main/java/com/atguigu/springboot/SpringBoot06DataMybatisApplication.java
@@ -1,8 +1,10 @@
 package com.atguigu.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = "com/atguigu/springboot/mapper")//扫描某个路径下的映射类，路径可以使用IDEA右键copy里的copypath
 @SpringBootApplication
 public class SpringBoot06DataMybatisApplication {
 
