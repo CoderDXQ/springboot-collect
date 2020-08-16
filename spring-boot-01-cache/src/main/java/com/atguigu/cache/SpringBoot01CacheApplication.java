eb5980ba71ce7824929be6d535b2d4259ea18aea
cococolabear <794055465@qq.com>
2020-08-16 19:12:07 +0800

    访问本地数据库

--- spring-boot-01-cache/src/main/java/com/atguigu/cache/SpringBoot01CacheApplication.java
+++ spring-boot-01-cache/src/main/java/com/atguigu/cache/SpringBoot01CacheApplication.java
@@ -1,8 +1,10 @@
 package com.atguigu.cache;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.atguigu.cache.mapper")//Mybatis的扫描路径
 @SpringBootApplication
 public class SpringBoot01CacheApplication {
 
--- spring-boot-01-cache/src/test/java/com/atguigu/cache/SpringBoot01CacheApplicationTests.java
+++ spring-boot-01-cache/src/test/java/com/atguigu/cache/SpringBoot01CacheApplicationTests.java
@@ -1,13 +1,21 @@
 package com.atguigu.cache;
 
+import com.atguigu.cache.bean.Employee;
+import com.atguigu.cache.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBoot01CacheApplicationTests {
+    @Autowired
+    EmployeeMapper employeeMapper;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+
+        Employee empById = employeeMapper.getEmpById(1);
+        System.out.println(empById);
     }
 
 }
