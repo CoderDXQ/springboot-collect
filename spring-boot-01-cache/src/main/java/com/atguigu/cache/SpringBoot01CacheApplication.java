49033464783dcb0a016337642e28f7507c082e42
cococolabear <794055465@qq.com>
2020-08-16 19:59:56 +0800

    开启缓存Cacheable和Enablecaching

--- spring-boot-01-cache/src/main/java/com/atguigu/cache/SpringBoot01CacheApplication.java
+++ spring-boot-01-cache/src/main/java/com/atguigu/cache/SpringBoot01CacheApplication.java
@@ -3,9 +3,11 @@ package com.atguigu.cache;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @MapperScan("com.atguigu.cache.mapper")//Mybatis的扫描路径
 @SpringBootApplication
+@EnableCaching//开启缓存
 public class SpringBoot01CacheApplication {
 
     public static void main(String[] args) {
--- spring-boot-01-cache/src/main/java/com/atguigu/cache/service/EmployeeService.java
+++ spring-boot-01-cache/src/main/java/com/atguigu/cache/service/EmployeeService.java
@@ -3,6 +3,7 @@ package com.atguigu.cache.service;
 import com.atguigu.cache.bean.Employee;
 import com.atguigu.cache.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +11,7 @@ public class EmployeeService {
     @Autowired
     EmployeeMapper employeeMapper;
 
+    @Cacheable(cacheNames = {"emp"})//将查询的返回结果进行缓存
     public Employee getEmp(Integer id){
         System.out.println("查询"+id+"号员工");
         Employee emp = employeeMapper.getEmpById(id);
