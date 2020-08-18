2f33f293a027516a04e2985bd730f9fc2ab49f3d
cococolabear <794055465@qq.com>
2020-08-18 21:23:07 +0800

    序列化机制

--- spring-boot-01-cache/src/test/java/com/atguigu/cache/SpringBoot01CacheApplicationTests.java
+++ spring-boot-01-cache/src/test/java/com/atguigu/cache/SpringBoot01CacheApplicationTests.java
@@ -21,7 +21,7 @@ class SpringBoot01CacheApplicationTests {
     StringRedisTemplate stringRedisTemplate;//k-v都是字符串
 
     @Autowired
-    RedisTemplate redisTemplate;//k-v都是对象
+    RedisTemplate empRedisTemplate;//k-v都是对象
 
     @Test
     public void test01(){
@@ -31,6 +31,20 @@ class SpringBoot01CacheApplicationTests {
 //        System.out.println(msg);
 //        stringRedisTemplate.opsForList().leftPush("mylist","1");
 //        stringRedisTemplate.opsForList().leftPush("mylist","2");
+
+    }
+
+    //测试保存对象
+    @Test
+    public void test02(){
+
+        Employee empById = employeeMapper.getEmpById(1);
+        //默认如果保存对象，使用jdk序列化机制，序列化之后的数据保存到redis中
+//        System.out.println(empById);
+//        redisTemplate.opsForValue().set("emp-01",empById);
+        System.out.println(empById);
+        //将序列化对象转化为json保存在redis中 改变默认的序列化规则
+        empRedisTemplate.opsForValue().set("emp-01",empById);
 
     }
 
