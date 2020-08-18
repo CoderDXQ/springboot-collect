6feb843ecba93da002339487b9178a021e6ada4c
cococolabear <794055465@qq.com>
2020-08-18 22:36:39 +0800

    自定义缓存规则

--- spring-boot-01-cache/src/main/java/com/atguigu/cache/config/MyRedisConfig.java
+++ spring-boot-01-cache/src/main/java/com/atguigu/cache/config/MyRedisConfig.java
@@ -3,9 +3,12 @@ package com.atguigu.cache.config;
 import com.atguigu.cache.bean.Employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.net.UnknownHostException;
 
@@ -20,4 +23,49 @@ public class MyRedisConfig {
         template.setDefaultSerializer(ser);
         return template;
     }
+
+    //CacheManagerCustomizers可以来定制缓存的一些规则
+//    @Bean
+//    public RedisCacheManager employeeCacheManager(RedisTemplate<Object,Employee> empRedisTemplate){
+//        RedisCacheManager cacheManager = new RedisCacheManager(empRedisTemplate);
+//        cacheManager.setUsePrefix(true);
+//
+//        return cacheManager;
+//    }
+
+
+//CacheManagerCustomizers可以来定制缓存的一些规则
+    @Bean
+    RedisCacheManager employeeRedisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer(Employee.class)))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer(Employee.class)))
+                .disableCachingNullValues(); RedisCacheManager redisCacheManager = RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .transactionAware()
+                .build();
+
+        return redisCacheManager;
+
+    }
+
+    /*
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+//初始化一个RedisCacheWriter
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
+//设置CacheManager的值序列化方式为json序列化
+        RedisSerializer<Object> jsonSerializer = new GenericJackson2JsonRedisSerializer();
+        RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair
+                .fromSerializer(jsonSerializer);
+        RedisCacheConfiguration defaultCacheConfig= RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(pair);
+//设置默认超过期时间是30秒
+        defaultCacheConfig.entryTtl(Duration.ofSeconds(30));
+//初始化RedisCacheManager
+        return new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
+    }
+    */
+
 }
