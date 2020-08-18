720ce1676f99ec033ab4de009914fcd8fb1e7999
cococolabear <794055465@qq.com>
2020-08-18 20:44:38 +0800

    RedisTemplate

--- spring-boot-01-cache/src/test/java/com/atguigu/cache/SpringBoot01CacheApplicationTests.java
+++ spring-boot-01-cache/src/test/java/com/atguigu/cache/SpringBoot01CacheApplicationTests.java
@@ -3,13 +3,36 @@ package com.atguigu.cache;
 import com.atguigu.cache.bean.Employee;
 import com.atguigu.cache.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBoot01CacheApplicationTests {
     @Autowired
     EmployeeMapper employeeMapper;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;//k-v都是字符串
+
+    @Autowired
+    RedisTemplate redisTemplate;//k-v都是对象
+
+    @Test
+    public void test01(){
+        //给redis中保存数据
+//        stringRedisTemplate.opsForValue().append("msg","hello");
+//        String msg = stringRedisTemplate.opsForValue().get("msg");
+//        System.out.println(msg);
+//        stringRedisTemplate.opsForList().leftPush("mylist","1");
+//        stringRedisTemplate.opsForList().leftPush("mylist","2");
+
+    }
 
     @Test
     public void contextLoads() {
