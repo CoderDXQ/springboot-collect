b11960c52c28cc5bcca4fd810cf63ac06bc57bb8
cococolabear <794055465@qq.com>
2020-08-19 17:17:52 +0800

    序列化和反序列化

--- springboot-02-amqp/src/test/java/com/atguigu/amqp/Springboot02AmqpApplicationTests.java
+++ springboot-02-amqp/src/test/java/com/atguigu/amqp/Springboot02AmqpApplicationTests.java
@@ -1,5 +1,6 @@
 package com.atguigu.amqp;
 
+import com.atguigu.amqp.bean.Book;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +21,7 @@ public class Springboot02AmqpApplicationTests {
     RabbitTemplate rabbitTemplate;
 
 
-    @Test//发送数据
+    @Test//发送数据 单播
     public void contextLoads() {
         //Message需要自己构造一个；定义消息体内容和消息头
         //rabbitTemplate.send(exchange,routeKey,message);
@@ -33,11 +35,28 @@ public class Springboot02AmqpApplicationTests {
         rabbitTemplate.convertAndSend("exchange.direct","atguigu.news",map);
     }
 
+    @Test//发送数据
+    public void contextLoads1() {
+        Map<String,Object> map = new HashMap<>();
+        map.put("msg","这是第一个消息");
+        map.put("data", Arrays.asList("helloworld",123,true));
+        //对象被默认序列化之后发送出去
+        rabbitTemplate.convertAndSend("exchange.direct","atguigu.news",new Book("书名","作者"));
+    }
+
     @Test//接收数据，消息队列中有数据才能接收，否则要先往消息队列中发送一条消息，接收后消息队列少一条消息
     public void receive(){
         Object o = rabbitTemplate.receiveAndConvert("atguigu.news");
         System.out.println(o.getClass());
         System.out.println(o);
     }
+
+    @org.junit.Test
+    public void sendMsg(){
+        rabbitTemplate.convertAndSend("exchange.fanout","",new Book("shuming","zuozhe"));
+    }
+
+
+
 
 }
