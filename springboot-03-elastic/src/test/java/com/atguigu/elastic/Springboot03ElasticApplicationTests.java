dde23cbedfaeccc938c0a7b3833cf2dff0d980a0
cococolabear <794055465@qq.com>
2020-08-20 23:25:15 +0800

    add

--- springboot-03-elastic/src/test/java/com/atguigu/elastic/Springboot03ElasticApplicationTests.java
+++ springboot-03-elastic/src/test/java/com/atguigu/elastic/Springboot03ElasticApplicationTests.java
@@ -4,9 +4,17 @@ import com.atguigu.elastic.bean.Article;
 import io.searchbox.client.JestClient;
 import io.searchbox.core.Index;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
+
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class Springboot03ElasticApplicationTests {
 
@@ -15,13 +23,22 @@ class Springboot03ElasticApplicationTests {
 
     @Test
     public void contextLoads() {
+        //给ES索引保存一个文档
         Article article = new Article();
         article.setId(1);
         article.setTitle("好消息");
         article.setAuthor("zhangsan");
         article.setContent("Hello World");
 
+        //构建一个索引功能
         Index index = new Index.Builder(article).index("atguigu").type("news").build();
+
+        try{//客户端执行索引
+            jestClient.execute(index);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
