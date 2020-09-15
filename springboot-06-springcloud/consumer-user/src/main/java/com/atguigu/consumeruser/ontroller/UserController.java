79a715ffca04e66256278576f053657d04b7a0bb
cococolabear <794055465@qq.com>
2020-09-16 01:50:44 +0800

    服务发现和消费

--- springboot-06-springcloud/consumer-user/src/main/java/com/atguigu/consumeruser/ontroller/UserController.java
+++ springboot-06-springcloud/consumer-user/src/main/java/com/atguigu/consumeruser/ontroller/UserController.java
@@ -1,2 +1,20 @@
-package com.atguigu.consumeruser.ontroller;public class UserController {
+package com.atguigu.consumeruser.ontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {//可以认为这个类是在controller层实现了service层的某些类的功能
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/buy")//可以传入字符串
+    public String buyTicket(String name){
+        String s = restTemplate.getForObject("http://PROVIDER-TICKET/ticket", String.class);
+        return name+"购买了"+s;
+    }
+
 }
