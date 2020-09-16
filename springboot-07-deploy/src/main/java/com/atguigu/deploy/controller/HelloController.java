abb5a5bdd54f4988481cf4f0cdef0dbeaa7f9cd3
cococolabear <794055465@qq.com>
2020-09-17 00:08:43 +0800

    add

--- springboot-07-deploy/src/main/java/com/atguigu/deploy/controller/HelloController.java
+++ springboot-07-deploy/src/main/java/com/atguigu/deploy/controller/HelloController.java
@@ -2,11 +2,13 @@ package com.atguigu.deploy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+//@RestController
 @Controller
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
